f356b863b44c4fc0f0690c131a72e3e0f2fd312c
Oleg010-web <nesteroff_oleg@mail.ru>
2023-01-19 11:47:40 +0700

    create-main-pages-for-app

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,17 @@
 <template>
   <q-page class="flex">
-
+    <h5>Users page</h5>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+//import { defineComponent } from 'vue'
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+
+// const mainComponent = defineComponent({
+//   name: 'IndexPage'
+// })
 </script>
+
+<style>
+</style>
