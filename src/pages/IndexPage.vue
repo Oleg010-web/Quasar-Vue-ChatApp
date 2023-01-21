3662fb38a4e3097217857e409a8d07947b4c6570
Oleg010-web <nesteroff_oleg@mail.ru>
2023-01-21 12:53:42 +0700

    Add main sections in Users page + data in script section from quasar

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,16 +1,49 @@
 <template>
-  <q-page class="flex">
-    <h5>Users page</h5>
+  <q-page class="flex q-pa-md">
+    <q-list 
+    class="full-width"
+    separator=""
+    >
+      <q-item v-for="user in users" :key="user.id" clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">
+            {{ user.name.charAt(0)}}
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-icon name="chat_bubble" color="green" />
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
-<script setup>
-//import { defineComponent } from 'vue'
+<script>
 
-
-// const mainComponent = defineComponent({
-//   name: 'IndexPage'
-// })
+export default {
+  setup () {
+    return {
+        users : [ {
+          id: 1,
+          name: 'Ruddy Jedrzej',
+        }, {
+          id: 2,
+          name: 'Mallorie Alessandrini',
+        }, {
+          id: 3,
+          name: 'Elisabetta Wicklen',
+        }, {
+          id: 4,
+          name: 'Seka Fawdrey',
+        } ]
+      }
+    }
+  }
 </script>
 
 <style>
