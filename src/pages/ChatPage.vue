883c145ab410699ecbe64ec3c978de88bb3ad313
Oleg010-web <nesteroff_oleg@mail.ru>
2023-01-23 12:39:39 +0700

    Design footer on chat page, added message input in footer

--- src/pages/ChatPage.vue
+++ src/pages/ChatPage.vue
@@ -1,6 +1,26 @@
 <template>
   <q-page class="flex">
-    <h5>Chat page</h5>
+    <q-footer elevated>
+        <q-toolbar>
+          <q-input 
+            bg-color="white" 
+            class="full-width"
+            outlined 
+            rounded 
+            v-model="newMessge" 
+            label="Message" 
+            dense>
+            <template v-slot:after>
+              <q-btn 
+                round
+                color="white" 
+                dense 
+                flat 
+                icon="send" />
+            </template>
+          </q-input>
+        </q-toolbar>
+      </q-footer>
   </q-page>
 </template>
 
