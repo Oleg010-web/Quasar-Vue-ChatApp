c726af82bbab81383adcd283250ab1a9af6090fd
Oleg010-web <nesteroff_oleg@mail.ru>
2023-01-23 13:04:57 +0700

    Added a main chat div

--- src/pages/ChatPage.vue
+++ src/pages/ChatPage.vue
@@ -1,5 +1,16 @@
 <template>
-  <q-page class="flex">
+  <q-page class="flex" column>
+    <div class="q-pa-md column col justify-end">
+      <q-chat-message
+        name="me"
+        :text="['hey, how are you?']"
+        sent
+      />
+      <q-chat-message
+        name="Jane"
+        :text="['doing fine, how r you?']"
+      />
+    </div>
     <q-footer elevated>
         <q-toolbar>
           <q-input 
@@ -24,8 +35,14 @@
   </q-page>
 </template>
 
-<script setup>
-
+<script>
+ export default {
+  data() {
+    return {
+      newMessage: ''
+    }
+  },
+ }
 
 
 </script>
