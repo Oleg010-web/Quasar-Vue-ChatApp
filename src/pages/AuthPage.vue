7da9c5297efe86d1afc91bf7263aa236f1348a51
Oleg010-web <nesteroff_oleg@mail.ru>
2023-01-24 14:03:05 +0700

    Main design settings fot AuthPage

--- src/pages/AuthPage.vue
+++ src/pages/AuthPage.vue
@@ -1,12 +1,46 @@
 <template>
-  <q-page class="flex">
-    <h5>Auth page</h5>
+  <q-page class="flex q-pa-md">
+    <q-card class="full-width">
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="login" label="Login" />
+          <q-tab name="register" label="Register" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="login">
+            <h5>Login</h5>
+          </q-tab-panel>
+
+          <q-tab-panel name="register">
+            <h5>Register</h5>
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
   </q-page>
 </template>
 
-<script setup>
+<script>
+  // imports
+  import { ref } from 'vue'
 
-
+  // data
+  export default {
+    setup () {
+      return {
+        tab: ref('login')
+      }
+    }
+  }
 
 
 </script>
