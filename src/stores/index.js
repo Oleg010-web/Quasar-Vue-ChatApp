06275276e820711f89ec6e4a1e10a00046bdb88a
Oleg010-web <nesteroff_oleg@mail.ru>
2023-01-25 17:46:50 +0700

    Added firebase for our app, final settings to work with data

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,10 +1,8 @@
 import { store } from 'quasar/wrappers'
-import { createPinia } from 'pinia'
 import Vuex from 'vuex'
 import  state from './store'
 import { Vue } from 'vue-demi'
 
-Vue.use(Vuex)
 
 /*
  * If not building with SSR mode, you can
